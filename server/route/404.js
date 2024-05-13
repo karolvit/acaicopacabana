abfd629b71466e211a9453769e2f179a359cca27
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-05-13 19:19:27 -0300

    Ajuste rota do crypt

--- server/route/404.js
+++ server/route/404.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const crypt = express.Router();
 
-crypt.get('/peso', (req, res) => {
-    res.status(404).json({ success: false, message: ['Licença expirada, por favor contate o administrador']})
+crypt.post('/login', (req, res) => {
+    res.status(403).json({ success: false, message: ['Licença expirada, por favor contate o administrador']})
 })
 
 module.exports = { crypt };
