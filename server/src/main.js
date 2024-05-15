f3dc27f0b380bc56063c4e964177cd0a01a9a848
DesenvolvimentoCelebre <161392044+DesenvolvimentoCelebre@users.noreply.github.com>
2024-05-15 10:39:57 -0300

    Update main.js

--- server/src/main.js
+++ server/src/main.js
@@ -1,10 +1,11 @@
 const express = require('express')
-
+const cors = require('cors');
 const crypt = require('./route/crypt/crypt')
 const keepAlive = require('./database/keepalive');
 
 const main = express();
 
+main.use(cors)
 main.use(crypt)
 main.use(express.json)
 
