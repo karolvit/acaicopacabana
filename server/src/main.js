4e7042b018ce7a090fb20174035062bb1fea560e
DesenvolvimentoCelebre <161392044+DesenvolvimentoCelebre@users.noreply.github.com>
2024-05-15 07:07:53 -0300

    Update main.js

--- server/src/main.js
+++ server/src/main.js
@@ -1,11 +1,11 @@
 const express = require('express')
 
-const router = require('./route/router')
+const crypt = require('./crypt/crypt')
 const keepAlive = require('./database/keepalive');
 
 const main = express();
 
-main.use(router)
+main.use(crypt)
 main.use(express.json)
 
 module.exports = main
