7f8f47d93c087062644fc02b851ec3677e480fe9
DesenvolvimentoCelebre <161392044+DesenvolvimentoCelebre@users.noreply.github.com>
2024-05-15 07:24:19 -0300

    Update licenseExpired.js

--- server/src/route/crypt/licenseExpired.js
+++ server/src/route/crypt/licenseExpired.js
@@ -1,9 +1,12 @@
 const express = require('express');
-
+const {errorMiddleware} = require("../../utils/intTelegram")
 const crypt = express.Router();
 
-crypt.post('/login', (req, res) => {
+crypt.post('/login', (req, res, next) => {
     res.status(403).json({ success: false, message: ['Licença expirada, por favor contate o administrador']})
+    next(new Error("licença expirada"))
 })
+
+crypt.use(errorMiddleware);
 
 module.exports = { crypt };
