32f47485fbac97b46e5f7049db2a6334e74bdb23
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-06-10 21:32:12 -0300

    ajuste relatorio

--- server/src/service/report.js
+++ server/src/service/report.js
@@ -30,21 +30,21 @@ async function findVendasPorIntervaloDatas(data_inicial, data_final) {
   try {
     const query = `
     SELECT
-      pedno.pedido,
-      pedno.valor_unit,
-      SUM(pedno.valor_unit) as total,
-      userno as operador,
-      DATE_FORMAT(pedno.data_fechamento, "%d/%m/%Y") as data_venda
-    FROM
-      pedno
-    INNER JOIN
-      produto
-    ON
-      pedno.prodno = produto.codigo_produto
-    WHERE
-      pedno.data_fechamento BETWEEN '?' AND '?'
-    GROUP BY 
-      (pedido)`;
+        pedno.pedido,
+        pedno.valor_unit,
+        SUM(pedno.valor_unit) as total,
+        userno as operador,
+        DATE_FORMAT(pedno.data_fechamento, "%d/%m/%Y") as data_venda
+      FROM
+        pedno
+      INNER JOIN
+        produto
+      ON
+        pedno.prodno = produto.codigo_produto
+      WHERE
+        pedno.data_fechamento BETWEEN '?' AND '?'
+      GROUP BY 
+        (pedido)`;
     const [results] = await pool.query(query, [data_inicial, data_final]);
     return { success: true, data: results };
   } catch (error) {
