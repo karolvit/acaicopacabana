69bbfb7aa2d5c2393e9f604809133e5cc9ba3cc0
Caique Carvalho <caique.carvalho@baianao.com.br>
2024-07-31 10:16:04 -0300

    Adicao de rota para verificar saldo

--- server/src/routes/system/system.js
+++ server/src/routes/system/system.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { getcaixa } = require('../../service/system');
+const { getcaixa, saldo } = require('../../service/system');
 const { errorMiddleware } = require('../../utils/intTelegram');
 
 const system = express.Router();
@@ -15,7 +15,21 @@ system.get("/gcx", async (req, res) => {
         res.status(500).json(result);
       }
     } catch (error) {
-      res.status(500).json({ success: false, error: "Erro interno do servidor", details: error });
+      res.status(500).json({ success: false, error: ["Erro interno do servidor", error]});
+    }
+});
+
+system.get("/sd", async (req, res) => {
+    try {
+      const result = await saldo();
+      
+      if (result.success) {
+        res.status(200).json(result);
+      } else {
+        res.status(500).json(result);
+      }
+    } catch (error) {
+      res.status(500).json({ success: false, error: ["Erro interno do servidor", error]});
     }
 });
   
--- server/src/service/system.js
+++ server/src/service/system.js
@@ -1,10 +1,36 @@
 const pool  = require('../database/connection');
 
-async function getcaixa({s0, sd}) {
+async function getcaixa() {
     try {
       const query = `SELECT s0 FROM cxlog WHERE date =  CURDATE()`;
 
-      const [results] = await pool.query(query, values);
+      const [results] = await pool.query(query);
+  
+      if (results.length === 0) {
+        return { success: true, message: "Não foi encontrado nenhum produto com esse nome" };
+      } else {
+        return { success: true, message: results };
+      }
+    } catch (error) {
+      return { success: false, error: "Erro no servidor, por favor contatar o administrador", details: error };
+    }
+  }
+
+async function saldo() {
+  try {
+    const query = `
+SELECT
+	sum(pedno.valor_unit) as saldo
+FROM 
+	pedno
+INNER JOIN
+	pay
+ON
+	pedno.pedido = pay.pedido
+WHERE
+	pedno.bit2 = 1 AND pay.tipo = 0`;
+
+  const [results] = await pool.query(query);
   
       if (results.length === 0) {
         return { success: true, message: "Não foi encontrado nenhum produto com esse nome" };
@@ -17,5 +43,6 @@ async function getcaixa({s0, sd}) {
   }
 
 module.exports = {
-    getcaixa
+    getcaixa,
+    saldo
 }
